b41104e9d3f78891e37db4bd0768bace07826926
Mrunal Patel <mrunalp@gmail.com>
2021-06-18 15:51:38 -0700

    Make testlog tolerate broken pipe
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- src/testlog/main.rs
+++ src/testlog/main.rs
@@ -1,10 +1,18 @@
 use std::{thread, time};
+use std::io;
+use std::io::ErrorKind;
+use std::io::Write;
 use chrono::Utc;
 
 fn main() {
     let one_second =  time::Duration::from_secs(1);
+    let mut stdout = io::stdout();
     loop {
-        println!("{:?}: Hello stdout!", Utc::now());
+        if let Err(e) = writeln!(stdout, "Hello stdout!") {
+            if e.kind() != ErrorKind::BrokenPipe {
+                eprintln!("Broken pipe!");
+            }
+        }
         thread::sleep(one_second);
         eprintln!("{:?}: Hello stderr!", Utc::now());
         thread::sleep(one_second);
